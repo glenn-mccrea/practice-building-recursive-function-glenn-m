0cb6e1dab69b82c489ce16e8ea112e714b3d71be
Glenn McCrea <glennmccrea@icloud.com>
2025-08-11 17:22:23 -0500

    cleaning

--- main.js
+++ main.js
@@ -15,32 +15,30 @@ reverseString("a"); // Outputs: "a"
 reverseString(""); // Outputs: ""
  */
 
-
 function reverseString(string) {
-    if
-        //base case to stop
-        (string.length <= 1) {
-        return string;
-    }
-    else {
-        //return console.log(`"${string.split("").reverse().join("")}"`); -- does it non recursicvely... linear i guess?
-        
-        //recursing...
-        // first we return the character at the end of the string. the number is "-1" because the length and the index difference.
-        return (string.charAt(string.length - 1) // results in "o" (last letter in 'hello')
-            //then we call the functiion again but this time with the "o" missing
-            + reverseString(string.slice(0, -1))); // results with 'hell' on this first recursion. then each time we take off a letter and put it "into" the function.
-        
-        //LEARNING NOTE/ reminder: -- the "-1" in .slice tells it to go to the end but to not include the last index.
-     }
+  if (
+    //base case to stop
+    string.length <= 1
+  ) {
+    return string;
+  } else {
+    //return console.log(`"${string.split("").reverse().join("")}"`); -- does it non recursicvely... linear i guess?
+
+    //recursing...
+    // first we return the character at the end of the string. the number is "-1" because the length and the index difference.
+    return (
+      string.charAt(string.length--) + // results in "o" (last letter in 'hello')
+      //then we call the functiion again but this time with the "o" missing
+      reverseString(string.slice(0, -1))
+    ); // results with 'hell' on this first recursion. then each time we take off a letter and put it "into" the function result.
+
+    //LEARNING NOTE/ reminder: -- the "-1" in .slice tells it to go to the end but to not include the last index.
+  }
 }
 console.log(reverseString("hello"));
 console.log(reverseString("recursion"));
 console.log(reverseString("a"));
-console.log(reverseString("")); 
-
-
-
+console.log(reverseString(""));
 
 /* First Call: reverseString("hello")
 
@@ -66,14 +64,12 @@ Fifth Call: reverseString("h")
 
     Base case reached (length is 1), returns "h" */
 
-
 //Then as it unwinds, we get:
 
-  /*   Fifth Call returns: "h"
+/*   Fifth Call returns: "h"
     Fourth Call returns: "e" + "h" → "eh"
     Third Call returns: "l" + "eh" → "leh"
     Second Call returns: "l" + "leh" → "lleh"
     First Call returns: "o" + "lleh" → "olleh" */
 
- 
-
+//REMEMBER LIFO - Last in, First out.
