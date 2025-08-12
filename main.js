bdb46276a6e6baee7aeb26f6a169342b7427bb31
Glenn McCrea <glennmccrea@icloud.com>
2025-08-12 09:05:37 -0500

    update

--- main.js
+++ main.js
@@ -7,12 +7,6 @@
         of the string with the result of the recursive call on the remaining part
         of the string (recursive case).
 3. Test the function with various string inputs to verify its correctness.
-
-Example Inputs and Outputs:
-reverseString("hello"); // Outputs: "olleh"
-reverseString("recursion"); // Outputs: "noisrucer"
-reverseString("a"); // Outputs: "a"
-reverseString(""); // Outputs: ""
  */
 
 function reverseString(string) {
@@ -22,13 +16,10 @@ function reverseString(string) {
   ) {
     return string;
   } else {
-    //return console.log(`"${string.split("").reverse().join("")}"`); -- does it non recursicvely... linear i guess?
-
     //recursing...
     // first we return the character at the end of the string. the number is "-1" because the length and the index difference.
     return (
-      string.charAt(string.length--) + // results in "o" (last letter in 'hello')
-      //then we call the functiion again but this time with the "o" missing
+      string.charAt(string.length - 1) + // results in "o" (last letter in 'hello') then we call the functiion again but this time with the "o" missing
       reverseString(string.slice(0, -1))
     ); // results with 'hell' on this first recursion. then each time we take off a letter and put it "into" the function result.
 
